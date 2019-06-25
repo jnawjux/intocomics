e73694fcff264a05bc5e0690c730f3fc4733c335
John Naujoks <johnnaujoks@gmail.com>
2019-06-24 19:04:49 -0700

    Add updated csv export of reviews

--- batcave.py
+++ batcave.py
@@ -16,3 +16,21 @@ def get_amazon_list_ids(link):
         for x in browser.find_elements_by_xpath('//*[@id="zg-ordered-list"]/li/span/div/span/a')]
 
     return all_ids
+
+
+def get_related_ids(df):
+    """Function to unpack a list of the unique related product ASINs from the 'Also bought' section.
+    Args:
+        df: Pandas dataframe (specifically from metadata.json as it works with its schema) 
+    Returns:
+        all_unique_ids: Python list of unique ASINs from a dataframe that are in the 'related' field
+    """
+
+    # Get each item from the sublist (related->also bought) and add to one list if not empty
+    all_ids = [val for meta in df.related.tolist() \
+                        for val in meta[0] if meta[0] is not None]
+
+    # Condense to a list of unique ids to eliminate any overlap
+    all_unique_ids = list(set(all_ids))
+
+    return all_unique_ids
