3294216f923f9415ea0525d803ad450e1c2db29c
John Naujoks <johnnaujoks@gmail.com>
2019-07-03 11:21:42 -0700

    Remove unneeded images, update flask app script

--- app.py
+++ app.py
@@ -1,26 +1,60 @@
 import random
 import pandas as pd
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.naive_bayes import MultinomialNB
-from sklearn.pipeline import Pipeline
-import pickle
+from selenium.webdriver import Chrome
+import pyspark
+import pyspark.sql.functions as F
+from pyspark.sql import Row
+from pyspark.ml.recommendation import ALS
+
 from flask import Flask, request, render_template, jsonify
 
+# Load Spark session
+spark = (pyspark.sql.SparkSession.builder
+.master("local")
+.getOrCreate())
 
-with open('spam_model.pkl', 'rb') as f:
-    model = pickle.load(f)
+# Load and persist ratings set
+all_reviews = spark.read.json('data/all_reviews_fixed_titles.json')
+all_reviews.persist()
+
 app = Flask(__name__, static_url_path="")
 
 @app.route('/')
 def index():
     """Return the main page."""
-    return render_template('indexy.html')
+    return render_template('index.html')
 
+if __name__ == '__main_':
+    app.run(port=8080)    #note set to 8080!
 
-@app.route('/predict', methods=['GET', 'POST'])
-def predict():
-    """Generate new sentences and add to origianl sentence."""
-    data = request.json
-    prediction = model.predict_proba([data['user_input']])
-    return jsonify(new_text)
+@app.route("/process", methods = ["GET", "POST"] )
+def process_form():
 
+    # Create dataframe of user reviews
+    reviews = []
+    for k, v in request.form.items():
+        rating = {'user_id': 101, 'overall': int(v), 'item_id': int(k) }
+        reviews.append(rating)
+    user_df = spark.createDataFrame(Row(**x) for x in reviews)
+
+    # Combine user reviews with others and prep for modeling
+    ratings_all = all_reviews.select(['item_id','overall','user_id'])\
+                             .union(user_df)
+
+    # Create ALS model 
+    als = ALS(rank=50, regParam=0.01, maxIter=20,
+      userCol='user_id', itemCol='item_id', 
+      ratingCol='overall', nonnegative=True)
+    
+    als_model = als.fit(ratings_all)
+    
+    # Get recommendations for user and only return those that are comics & top three
+    user_recommend = als_model.recommendForAllUsers(50)
+    recs_for_user = user_recommend.where(user_recommend.user_id == 101).take(1)
+    all_comics = [reco[0] for reco in recs_for_user[0]['recommendations']\
+                  if str(reco[0]).endswith('22') ]
+    comic_titles = list(set(all_reviews.filter(F.col('item_id')\
+                                       .isin(all_comics))\
+                                       .select(['item_id', 'title']).collect()))
+
+    return render_template('index.html', comic_recs = comic_titles)
--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,5 +1,1 @@
 // Empty JS for your own code to be here
-$('.container').infiniteScroll({
-    append: '.more-movies',
-    status: '.page-load-status',
-  });
