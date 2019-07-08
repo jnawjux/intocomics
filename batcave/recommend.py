fda177a5a284e940565f6dc0b17e11dce171f62b
John Naujoks <johnnaujoks@gmail.com>
2019-07-07 21:31:43 -0700

    🛁 Lint my Python files

--- batcave/recommend.py
+++ batcave/recommend.py
@@ -2,14 +2,17 @@ import pandas as pd
 import numpy as np
 from IPython.display import clear_output
 
+
 def get_new_user_matrix(item_df, user_df, rank=50):
     """
-    Take in a new users ratings, align with the existing item factors, and return the user factors
+    Take in a new users ratings, align with the existing item
+    factors,and return the user factors
     Args:
-        item_df: item factors dataframe resulting from a Spark Alternating Least Squares (ALS) Model
+        item_df: item factors dataframe resulting from a Spark
+         Alternating Least Squares (ALS) Model
         user_df: Pandas dataframe with item_ids and ratings
     Returns:
-        new_user_matrix: np.array with calculated new users factors. 
+        new_user_matrix: np.array with calculated new users factors.
     """
     # User ratings array
     all_ratings_array = np.array((user_df.rating.tolist(),)).T
@@ -18,63 +21,76 @@ def get_new_user_matrix(item_df, user_df, rank=50):
     all_items_array = np.zeros(shape=(user_df.shape[0], rank))
 
     for index, item in user_df.iterrows():
-        all_items_array[index, :] = np.array(item_df.loc[item_df['item_id'] == item['item_id'],'features'].item())
-    
+        all_items_array[index, :] = np.array(
+            item_df.loc[item_df["item_id"] == item["item_id"],
+                        "features"].item()
+        )
+
     # Least squares solution to get user features
-    new_user_matrix = np.linalg.lstsq(all_items_array, all_ratings_array, rcond=None)
+    new_user_matrix = np.linalg.lstsq(all_items_array,
+                                      all_ratings_array,
+                                      rcond=None)
 
     # New users matrix!
     new_user_matrix = new_user_matrix[0].reshape((50,))
-    
+
     return new_user_matrix
 
 
 def get_user_reviews_testing(items_df):
-    """Take user input and create dataframe added for recommending, built for testing in Jupyter Notebook
+    """Take user input and create dataframe added for recommending,
+         built for testing in Jupyter Notebook
     Args:
         None
     Returns:
         pd.DataFrame(reviews): Pandas dataframe of users reviews from inputs
     """
     # Load data and get random sample of movies with more than 100 reviews
-    movie_rand_sample = items_df[(items_df['item_id'].astype(str).str.endswith('44')) &\
-                                        (items_df['count'] > 50)].sample(n=25)
-
+    movie_rand_sample = items_df[
+        (items_df["item_id"].astype(str).str.endswith("44"))
+        & (items_df["count"] > 50)
+    ].sample(n=25)
     reviews = []
-
     # Give a user input and movie title and take in score
     for index, movie in movie_rand_sample.iterrows():
-        print(movie['title'])
-        rating = input("How would you rate this movie? (0-5, OR type 'skip'): ")
+        print(movie["title"])
+        rating = input(
+            "How would you rate this movie? (0-5, OR type 'skip'): "
+            )
         # If user has not seen, can enter skip instead
-        if rating == 'skip':
+        if rating == "skip":
             clear_output()
             continue
         # Creating dictionary of review and adding to reviews
         else:
-            movie_rating = {'rating': int(rating),
-                            'item_id': movie['item_id'] }
+            movie_rating = {"rating": int(rating), "item_id": movie["item_id"]}
             reviews.append(movie_rating)
             clear_output()
-            if len(reviews) >=10:
+            if len(reviews) >= 10:
                 return pd.DataFrame(reviews)
             else:
                 continue
 
+
 def get_recommendations(item_factors_df, new_user_df):
-    """Get recommendations for new user, built for testing in Jupyter Notebook
+    """Get recommendations for new user,
+        built for testing in Jupyter Notebook
     Args:
-        new_user_df: Pandas dataframe with user reviews, as generated from get_user_reviews function
+        new_user_df: Pandas dataframe with user reviews,
+        as generated from get_user_reviews function
     Returns:
         Prints top three recommended comics for new user
     """
     user_matrix = get_new_user_matrix(item_factors_df, new_user_df)
 
-    item_factors_df['new_user_predictions'] = item_factors_df['features'].apply(lambda x: np.dot(x, user_matrix))
+    item_factors_df["new_user_predictions"] =
+    item_factors_df["features"].apply(
+            lambda x: np.dot(x, user_matrix)
+            )
 
-    top_five_comics = item_factors_df.loc[item_factors_df['item_id']\
-                              .astype(str).str.endswith('22'), ['item_id', 'title', 'asin', 'new_user_predictions']]\
-                              .sort_values('new_user_predictions', ascending=False)[:5]
+    top_five_comics = item_factors_df.loc[
+        item_factors_df["item_id"].astype(str).str.endswith("22"),
+        ["item_id", "title", "asin", "new_user_predictions"],
+    ].sort_values("new_user_predictions", ascending=False)[:5]
 
     return top_five_comics
-
--- batcave/scrape_and_clean.py
+++ batcave/scrape_and_clean.py
@@ -4,67 +4,85 @@ import pandas as pd
 import pyspark
 import urllib
 
+
 def get_amazon_list_ids(link):
-    """Scraping function using Selenium for getting Amazon product IDs (ASIN) from 'Best Seller' pages.
+    """Scraping function using Selenium for getting Amazon product IDs
+         (ASIN) from 'Best Seller' pages.
     Args:
-      link: html link of 'Best Sellers' page. 
+      link: html link of 'Best Sellers' page.
     Returns:
-      all_ids: a list of ASIN ids found on page. 
+      all_ids: a list of ASIN ids found on page.
     """
     # Instantiate Chrome and open to link
     browser = Chrome()
     browser.get(link)
 
-    # Grabs the links from the page, then seperates out the ASIN from the link for each product
-    all_ids = [x.get_attribute('href').split('dp/')[1].split('/')[0] 
-        for x in browser.find_elements_by_xpath('//*[@id="zg-ordered-list"]/li/span/div/span/a')]
+    # Grabs the links from the page, seperates out the ASIN from the link
+    all_ids = [
+        x.get_attribute("href").split("dp/")[1].split("/")[0]
+        for x in browser.find_elements_by_xpath(
+            '//*[@id="zg-ordered-list"]/li/span/div/span/a'
+        )
+    ]
 
     return all_ids
 
+
 def get_related_ids(df):
-    """Function to unpack a list of the unique related product ASINs from the 'Also bought' section.
+    """Function to unpack a list of the unique related product ASINs
+         from the 'Also bought' section.
     Args:
-        df: Pandas dataframe (specifically from metadata.json as it works with its schema) 
+        df: Pandas dataframe (specifically from metadata.json as it works
+         with its schema)
     Returns:
-        all_unique_ids: Python list of unique ASINs from a dataframe that are in the 'related' field
+        all_unique_ids: Python list of unique ASINs from a dataframe that
+         are in the 'related' field
     """
 
-    # Get each item from the sublist (related->also bought) and add to one list if not empty
-    all_ids = [val for meta in df.related.tolist() \
-                        for val in meta[0] if meta[0] is not None]
+    # Get each item from the sublist and add to one list if not empty
+    all_ids = [
+        val for meta in df.related.tolist()
+        for val in meta[0] if meta[0] is not None
+    ]
 
     # Condense to a list of unique ids to eliminate any overlap
     all_unique_ids = list(set(all_ids))
 
     return all_unique_ids
 
+
 def new_id_dictionary(df, column, suffix_val):
-    """Take in column with unique indexes, return dictionary with new index values. This is done to
-     remove the default ASIN and user ID from Amazon reviews and create better unique ids.
+    """Take in column with unique indexes, return dictionary with new
+     index values. This is done to remove the default ASIN and user ID
+     from Amazon reviews and create better unique ids.
     Args:
         df: source dataframe
         column: name of column with ids to replace
-        suffix_val: new suffix value for unique codes. Example: all new user_ids could end
-        with '00000'
+        suffix_val: new suffix value for unique codes.
+        (Example: all new user_ids could end with '00000')
     Returns:
         new_id_dict: New Spark dataframe with column of new unique ids
-    
     """
-    unique_vals = list(set([old_id[0] for old_id in df.select(column).collect()]))
-    new_ids = [(str(i) + suffix_val) for i in range(1,len(unique_vals)+1)]
-    new_id_dict = {k:v for k,v in zip(unique_vals, new_ids)}
+    unique_vals = list(
+                    set(
+                        [old_id[0] for old_id in df.select(column).collect()]
+                        )
+                    )
+    new_ids = [(str(i) + suffix_val) for i in range(1, len(unique_vals) + 1)]
+    new_id_dict = {k: v for k, v in zip(unique_vals, new_ids)}
     return new_id_dict
 
 
 def get_title_by_asin(asin):
-    """Scraping function using Selenium for getting product names by Amazon ASIN.
+    """Scraping function using Selenium for getting
+        product names by Amazon ASIN.
     Args:
-      asin: Amazon ASIN unique product ID 
+      asin: Amazon ASIN unique product ID
     Returns:
-      title: title of product on Amazon with that ASIN 
+      title: title of product on Amazon with that ASIN
     """
     # Create search result page from ASIN
-    link  = "https://www.amazon.com/s?k=" + asin + "&ref=nb_sb_noss"
+    link = "https://www.amazon.com/s?k=" + asin + "&ref=nb_sb_noss"
 
     # Instatiate browser
     browser = Chrome()
@@ -73,57 +91,64 @@ def get_title_by_asin(asin):
 
     # Find title by unique XPath to first result (most common format)
     try:
-        title = browser.find_element_by_xpath('//*[@id="search"]/div[1]/div[2]/div/span[3]/div[1]\
-                                            /div/div/div/div/div[2]/div[2]/div/div[1]/div/div/div\
-                                            [1]/h2/a/span').text
-    
-    except:
-        title = 'None found'
+        title = browser.find_element_by_xpath(
+            '//*[@id="search"]/div[1]/div[2]/div/span[3]/div[1]\
+            /div/div/div/div/div[2]/div[2]/div/div[1]/div/div/div\
+            [1]/h2/a/span'
+        ).text
+
+    except Exception:
+        title = "None found"
     browser.close()
     return title
 
 
 def get_missing_titles(asin_list):
-    """Take in a list of ASINs and return a list of dictionaries with their correct title
+    """Take in a list of ASINs and return a list of dictionaries
+        with their correct title
         Args:
       asin_list: list of ASINs to search for on Amazon
     Returns:
-      asin_title_list: a new list of dictionaries with the asin and title for each"""
+      asin_title_list: a new list of dictionaries
+        with the asin and title for each"""
     asin_title_list = []
 
-    # Run each ASIN through the get_title_by_asin function and return dictionary
     for asin in asin_list:
         title = get_title_by_asin(asin)
-        new_temp_dict = {'asin': asin, 'title': title}
+        new_temp_dict = {"asin": asin, "title": title}
         asin_title_list.append(new_temp_dict)
         time.sleep(5)
     return asin_title_list
 
+
 def get_item_image(df, directory):
-    """Get images from  dataframe. This is meant specifically to work with metadata dataframe 
+    """Get images from dataframe. This is meant specifically
+         to work with metadata dataframe
     Args:
         df: subset dataframe to extract from
     Returns:
         saves images to 'images/' folder
     """
     for index, item in df.iterrows():
-        url = item['imUrl']
+        url = item["imUrl"]
         # Concats items ASIN and original file extension to create name
-        filename = directory + str(item['item_id']) + item['imUrl'][-4:]
+        filename = directory + str(item["item_id"]) + item["imUrl"][-4:]
         try:
             urllib.request.urlretrieve(url, filename)
-        except:
+        except Exception:
             continue
 
+
 def row_to_html_card(row):
-    """Takes a row from a dataframe and returns the card html needed for the web app
+    """Takes a row from a dataframe and returns
+        the card html needed for the web app
     Args:
     row: individual row from Pandas dataframe with title, asin, and item_id
     Returns:
     card_html: html text for an html card"""
-    title = row['title']
-    filename = 'images/movies/' + str(row['asin']) + '.jpg'
-    item_id = str(row['item_id'])
+    title = row["title"]
+    filename = "images/movies/" + str(row["asin"]) + ".jpg"
+    item_id = str(row["item_id"])
     card_html = f"""
     <div class="col-md-3">
         <div class="card bg-light">
@@ -137,15 +162,20 @@ def row_to_html_card(row):
                     <div class="row">
                         <div class="mx-auto">
                             <input type="radio" id="star1" name="{item_id}"
-                                value=1 /><label for="star1" title="HATE">1</label>
+                            value=1 />
+                            <label for="star1" title="HATE">1</label>
                             <input type="radio" id="star2" name="{item_id}"
-                                value=2 /><label for="star2" title="Meh">2</label>
+                            value=2 />
+                            <label for="star2" title="Meh">2</label>
                             <input type="radio" id="star3" name="{item_id}"
-                                value=3 /><label for="star3" title="Just okay">3</label>
+                            value=3 />
+                            <label for="star3" title="Just okay">3</label>
                             <input type="radio" id="star4" name="{item_id}"
-                                value=4 /><label for="star4" title="Good">4</label>
+                            value=4 />
+                            <label for="star4" title="Good">4</label>
                             <input type="radio" id="star5" name="{item_id}"
-                                value=5 /><label for="star5" title="Love it!">5</label>
+                            value=5 />
+                            <label for="star5" title="Love it!">5</label>
                         </div>
                     </div>
                 </div>
